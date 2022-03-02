2db77a4cbc765fc3e1c39e901f512b212ab9a630
Sahilbalhra <sahilbalhra0@gmail.com>
2022-03-02 22:12:17 +0530

    changes in main and details files

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,22 @@
 import React from "react";
 import Home from "./Home/Home";
 import Navbar from "./NavBar/Navbar";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./Home/Detail";
 
 const Main = () => {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Home />
-    </>
+      <Switch>
+        <Route path="/detail">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
