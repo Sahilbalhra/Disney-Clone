827e23c944f3b4deeccc5cd27f119722aee2ca8b
Sahilbalhra <sahilbalhra0@gmail.com>
2022-03-03 13:59:20 +0530

    login page added

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,12 +3,16 @@ import Home from "./Home/Home";
 import Navbar from "./NavBar/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Detail from "./Home/Detail";
+import LogIn from "./Home/LogIn";
 
 const Main = () => {
   return (
     <Router>
       <Navbar />
       <Switch>
+        <Route path="/login">
+          <LogIn />
+        </Route>
         <Route path="/detail">
           <Detail />
         </Route>
